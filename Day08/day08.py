f21d8ff93f7576edc838a4a002d8e645b9b8e545
Perrine Cribier-Delande <perrine.cribier-delande@proton.me>
2024-12-08 14:16:51 +0100

    Day 08

--- Day08/day08.py
+++ Day08/day08.py
@@ -1,25 +1,75 @@
-def read_input(input_file: str) -> tuple:
+import numpy as np
+import itertools
+
+
+def read_input(input_file: str) -> tuple[np.ndarray[str, str], list[str]]:
     with open(input_file) as f:
         content = f.readlines()
+    
+    grid = np.array([list(cont) for cont in content])[:, :-1]
+    all_antena_type = [a[0] for a in list(np.unique(grid)) if a[0] != '.']
+    return grid, all_antena_type
 
 
-def level1() -> int:
-    res = 0
+def get_antinodes_from_two_antenas(antena1: tuple[int, int], antena2: tuple[int, int]) -> tuple[list[tuple[int, int]], list[tuple[int, int]]]:
+    x1, y1 = antena1
+    x2, y2 = antena2
+    diff_x = x1-x2
+    diff_y = y1-y2
+    possible1 = (x1+diff_x, y1+diff_y)
+    possible2 = (x2-diff_x, y2-diff_y)
 
-    return res
+    possible_antinodes1 = []
+    possible_antinodes2 = []
+    while check_antinode_in_grid(possible1, x_lim, y_lim):
+        possible_antinodes1.append(possible1)
+        possible1 = (possible1[0] + diff_x, possible1[1] + diff_y)
+    while check_antinode_in_grid(possible2, x_lim, y_lim):
+        possible_antinodes2.append(possible2)
+        possible2 = (possible2[0] - diff_x, possible2[1] - diff_y)
+    return (possible_antinodes1, possible_antinodes2)
 
 
-def level2() -> int:
-    res = 0
+def check_antinode_in_grid(antinode_coord: tuple[int, int], x_lim: int, y_lim: int) -> bool:
+    x, y = antinode_coord
+    return not (x < 0 or y < 0 or x >= x_lim or y >= y_lim)
 
-    return res
+
+def level1(grid: np.ndarray[str, str], all_antena_type: list[str], x_lim: int, y_lim: int) -> int:
+    antinodes = []
+    for antena_type in all_antena_type:
+        xs,ys = np.where(grid == antena_type)
+        antena_coords = [(int(x), int(y)) for x,y in zip(xs,ys)]
+        for antena_couple in list(itertools.combinations(antena_coords, 2)):
+            possible1, possible2 = get_antinodes_from_two_antenas(antena1=antena_couple[0], antena2=antena_couple[1])
+            if len(possible1) > 0:
+                 antinodes.append(possible1[0])
+            if len(possible2) > 0:
+                 antinodes.append(possible2[0])
+    return len(set(antinodes))
+
+
+def level2(grid: np.ndarray[str, str], all_antena_type: list[str], x_lim: int, y_lim: int) -> int:
+    antinodes = []
+    for antena_type in all_antena_type:
+        xs,ys = np.where(grid == antena_type)
+        antena_coords = [(int(x), int(y)) for x,y in zip(xs,ys)]
+        for antena_couple in list(itertools.combinations(antena_coords, 2)):
+            possible1, possible2 = get_antinodes_from_two_antenas(antena1=antena_couple[0], antena2=antena_couple[1])
+            antinodes += possible1
+            antinodes += possible2
+        if len(antena_coords) > 1:
+            antinodes += antena_coords
+    return len(set(antinodes))
+
 
 if __name__ == "__main__":
-    input_file = './Day08/input0.txt'
+    input_file = './Day08/input1.txt'
 
-    _ = read_input(input_file)
+    grid, all_antena_type = read_input(input_file)
+    x_lim, y_lim = grid.shape
 
-    print('level 1 : ', level1())
-    print('level 2 : ', level2())
+    print('level 1 : ', level1(grid, all_antena_type, x_lim, y_lim))
+    print('level 2 : ', level2(grid, all_antena_type, x_lim, y_lim))
 
 
