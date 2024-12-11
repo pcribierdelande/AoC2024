ba029c39b963e42d5a7d8855bda648cb19b4fdcd
Perrine Cribier-Delande <perrine.cribier-delande@dgccrf.finances.gouv.fr>
2024-12-11 23:22:47 +0100

    Day 10 and 11

--- Day09/day09.py
+++ Day09/day09.py
@@ -1,28 +1,125 @@
-def read_input(input_file: str) -> tuple:
-    with open(input_file, encoding='utf8') as f:
+def read_input(input_file: str) -> str:
+    with open(input_file) as f:
         content = f.readlines()
-    return content
+    return content[0]
 
 
-def level1() -> int:
-    res = 0
+def create_dic_disk_files(disk_files: str) -> dict[int, int]:
+    return {a: int(c) for a, c in enumerate(disk_files)}
 
+
+def move_file_by_parts(disk_files: dict[int, int], free_spaces: list[int]) -> list[int]:
+    hard_drive_res = [0] * int(disk_files[0])
+    i = 1
+
+    free_space = free_spaces.pop(0)
+    for a in range(len(disk_files) - 1, 1, -1):
+        if i >= a:
+            break
+        reste_a_remplir = disk_files[a]
+        while reste_a_remplir > 0:
+            if free_space >= reste_a_remplir:
+                hard_drive_res += [a] * reste_a_remplir
+                free_space -= reste_a_remplir
+                reste_a_remplir = 0
+            else:
+                hard_drive_res += [a] * free_space
+                reste_a_remplir -= free_space
+                free_space = free_spaces.pop(0)
+                if i == a:
+                    hard_drive_res += [i] * reste_a_remplir
+                    reste_a_remplir = 0
+                else:
+                    hard_drive_res += [i] * disk_files[i]
+
+                i += 1
+    return hard_drive_res
+
+
+def create_dic_empty_spaces(
+    disk_files: dict[int, int], free_spaces: list[int]
+) -> dict[int:int]:
+    dic_empty_space = {}
+    current = 0
+    for idx, (file, free) in enumerate(zip(disk_files.values(), free_spaces)):
+        current += file
+        dic_empty_space[idx] = current
+        current += free
+    return dic_empty_space
+
+
+def build_res(
+    disk_files: dict[int, int],
+    removed_files: dict[int, int],
+    dic_mvt: dict[int, int],
+    empty_or: list[int],
+) -> list[int]:
+    res = []
+    for idx, e in enumerate(empty_or):
+        if idx in disk_files:
+            res += [idx] * disk_files[idx]
+        else:
+            res += [0] * removed_files[idx]
+        res += [0] * e
+    for k, v in dic_mvt.items():
+        res[k] = v
     return res
 
 
-def level2() -> int:
-    res = 0
+def move_entire_file(disk_files: dict[int, int], free_spaces: list[int]) -> list[int]:
+    free_spaces_or = free_spaces.copy()
+    dic_empty_space = create_dic_empty_spaces(disk_files, free_spaces)
+    dic_mvt = {}
+    to_remove = []
+    for a in range(len(disk_files) - 1, 1, -1):
+        file_size = disk_files[a]
+        first_free_space = next(
+            (i for i, x in enumerate(free_spaces) if x >= file_size), -1
+        )
+        if first_free_space != -1 and first_free_space < a:
+            for i in range(file_size):
+                dic_mvt[dic_empty_space[first_free_space] + i] = a
+                free_spaces[first_free_space] -= 1
+            dic_empty_space[first_free_space] += file_size
+            to_remove.append(a)
 
-    return res
+    disk_result = {}
+    removed_files = {}
+    for k, v in disk_files.items():
+        if k in to_remove:
+            removed_files[k] = v
+        else:
+            disk_result[k] = v
+
+    return build_res(disk_result, removed_files, dic_mvt, free_spaces_or)
+
+
+def compute_checksum(disk_res: list[int]) -> int:
+    return sum(x * y for x, y in zip(disk_res, list(range(len(disk_res)))))
+
+
+def level1(line: str) -> int:
+    disk_files = line[::2]
+    free_spaces = [int(a) for a in line[1::2]]
+    disck_files_dict = create_dic_disk_files(disk_files)
+    return compute_checksum(move_file_by_parts(disck_files_dict, free_spaces))
+
+
+def level2(line: str) -> int:
+    disk_files = line[::2]
+    free_spaces = [int(a) for a in line[1::2]]
+    disck_files_dict = create_dic_disk_files(disk_files)
+
+    return compute_checksum(move_entire_file(disck_files_dict, free_spaces))
 
 
 def main():
-    input_file = "./Day09/input0.txt"
+    input_file = "./Day09/input1.txt"
 
-    _ = read_input(input_file)
+    line = read_input(input_file)
 
-    print("level 1 : ", level1())
-    print("level 2 : ", level2())
+    print("level 1 : ", level1(line))
+    print("level 2 : ", level2(line))
 
 
 if __name__ == "__main__":
---
+++ Day11/day11.py
@@ -0,0 +1,95 @@
+import math
+import itertools
+from operator import itemgetter
+
+
+def read_input(input_file: str) -> list[int]:
+    with open(input_file, encoding="utf8") as f:
+        content = f.readlines()
+    return [int(a) for a in content[0].split(" ")]
+
+
+def one_blink(stones_line: list[int]) -> list[int]:
+    new_line = []
+    for stone in stones_line:
+        if stone == 0:
+            new_line.append(1)
+        elif len(str(stone)) % 2 == 0:
+            stone_str = str(stone)
+            new_line.append(int(stone_str[: int(len(stone_str) / 2)]))
+            new_line.append(int(stone_str[int(len(stone_str) / 2) :]))
+        else:
+            new_line.append(stone * 2024)
+    return new_line
+
+
+def stones_after_blinks(stone: int, blinks: int) -> None:
+
+    if blinks == 1:
+        if (stone, 1) not in dict_stones:
+            dict_stones[(stone, 1)] = one_blink([stone])
+
+    if (stone, blinks) not in dict_stones:
+        next_stones = one_blink([stone])
+        dict_stones[(stone, blinks)] = [(a, blinks - 1) for a in next_stones]
+        for a in next_stones:
+            stones_after_blinks(a, blinks - 1)
+
+
+def fill_dict():
+    list_keys = list(dict_stones.keys())
+    list_keys.sort(key=itemgetter(1))
+    for key in list_keys:
+        if isinstance(dict_stones[key], int):
+            pass
+        elif isinstance(dict_stones[key][0], int):
+            dict_stones[key] = len(dict_stones[key])
+        elif isinstance(dict_stones[key][0], tuple):
+            dict_stones[key] = sum([dict_stones[a] for a in dict_stones[key]])
+        else:
+            print("aaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+
+# def build_dict():
+# for a in range(10):
+#     stones_line = [a]
+#     stones_line = one_blink(stones_line)
+#     dict_stones[(a, 1)] = stones_line
+# for a in range(10):
+#     stones_after_blinks(a, 30)
+
+
+def level1(stones_line: list[int]) -> int:
+    for _ in range(25):
+        stones_line = one_blink(stones_line)
+
+    return len(stones_line)
+
+
+def level2(stones_line) -> int:
+    res = 0
+    # build_dict()
+    for stone in stones_line:
+        stones_after_blinks(stone, 75)
+
+    fill_dict()
+
+    for stone in stones_line:
+        res += dict_stones[(stone, 75)]
+    return res
+    # return nbr_stones_after_blinks(0, 25)
+
+
+def main():
+    input_file = "./Day11/input1.txt"
+
+    stones_line = read_input(input_file)
+
+    print("level 1 : ", level1(stones_line))
+    print("level 2 : ", level2(stones_line))
+
+
+if __name__ == "__main__":
+    dict_stones = {}
+
+    main()
